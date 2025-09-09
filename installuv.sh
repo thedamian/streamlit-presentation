3c2f9c67596d2af319b1d783ae61bf4e6014fcf7
Damian Montero <photoca@hotmail.com>
2025-09-08 20:13:58 -0400

    making an advanced install.uv

--- installuv.sh
+++ installuv.sh
@@ -1,3 +1,9 @@
+echo "Installing/updating uv..."
 curl -LsSf https://astral.sh/uv/install.sh | sh
+echo "Creating/updating venv..."
 uv venv
-.venv/Scripts/activate
+echo "Activating venv..."
+ source .venv/bin/activate
+echo "Installing/updating dependencies..."
+uv sync
+echo "All done!"
